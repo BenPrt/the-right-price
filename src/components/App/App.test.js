1eeaf8e93ba15dc87fdf31d1ca73aabee25892c2
Ben <parant.b@gmail.com>
2020-04-05 15:50:07 +0200

    First commit : Initialization of the React/Redux Application

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import Store from 'src/redux/Store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Hello World"', () => {
   const { getByText } = render(
-    <Provider store={store}>
+    <Provider store={Store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText('Hello World')).toBeInTheDocument();
 });
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import SimpleReducer from './SimpleReducer';
+
+export default combineReducers({
+    SimpleReducer,
+});
