1d92e4e1f69afaad666aa76f63de4622e4c4c7a5
Ben <parant.b@gmail.com>
2020-04-09 10:00:10 +0200

    Improved splash screen animation

--- src/components/SplashScreen/SplashScreen.js
+++ src/components/SplashScreen/SplashScreen.js
@@ -27,78 +27,89 @@ function SplashScreen() {
   };
 
   return (
-    <div className={`SplashScreen ${isSplashScreenDisplayed ? '' : 'hidden'}`}>
-      <Container fluid>
-        <Row className="justify-content-center">
-          <Col xs={12} md={10} xl={6}>
-            <Container>
-              <Row className="justify-content-center">
-                <Col xs={6} md={3}>
-                  <img
-                    alt="Splash Screen Logo"
-                    src={SplashScreenLogo}
-                    id="splashscreen-logo"
-                  />
-                </Col>
-              </Row>
-              <Row className="justify-content-center">
-                <Col xs={7} md={4}>
-                  <h1 id="app-title">
-                    <span>The Right</span> Price
-                  </h1>
-                </Col>
-              </Row>
-              <Row className="justify-content-center">
-                <Col xs={8} md={6}>
-                  <p id="app-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo
-                    aenean accumsan, mattis nibh proin auctor.
-                  </p>
-                </Col>
-              </Row>
-              <Row className="justify-content-center">
-                <Col xs={10} md={6}>
-                  <FormControl
-                    variant="outlined"
-                    className="currency-select-form"
-                  >
-                    <InputLabel htmlFor="currency-select-field">
-                      Favorite Currency
-                    </InputLabel>
-                    <Select
-                      native
-                      value={currency}
-                      onChange={handleCurrencySelection}
-                      label="Favorite currency"
-                      inputProps={{
-                        name: 'currency',
-                        id: 'currency-select-field',
-                      }}
+    <div className="SplashScreen">
+      <div
+        className={`splash-screen-overlay ${
+          isSplashScreenDisplayed ? '' : 'hidden'
+        }`}
+      ></div>
+      <div
+        className={`splash-screen-frame ${
+          isSplashScreenDisplayed ? '' : 'hidden'
+        }`}
+      >
+        <Container fluid>
+          <Row className="justify-content-center">
+            <Col xs={12} md={10} xl={6}>
+              <Container>
+                <Row className="justify-content-center">
+                  <Col xs={6} md={3}>
+                    <img
+                      alt="Splash Screen Logo"
+                      src={SplashScreenLogo}
+                      id="splashscreen-logo"
+                    />
+                  </Col>
+                </Row>
+                <Row className="justify-content-center">
+                  <Col xs={7} md={4}>
+                    <h1 id="app-title">
+                      <span>The Right</span> Price
+                    </h1>
+                  </Col>
+                </Row>
+                <Row className="justify-content-center">
+                  <Col xs={8} md={6}>
+                    <p id="app-description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Leo aenean accumsan, mattis nibh proin auctor.
+                    </p>
+                  </Col>
+                </Row>
+                <Row className="justify-content-center">
+                  <Col xs={10} md={6}>
+                    <FormControl
+                      variant="outlined"
+                      className="currency-select-form"
                     >
-                      <option value={'USD'}>US Dollars</option>
-                      <option value={'CAD'}>Canadian Dollars</option>
-                      <option value={'EUR'}>Euros</option>
-                    </Select>
-                  </FormControl>
-                </Col>
-              </Row>
-              <Row className="justify-content-center">
-                <Col xs={10} md={6}>
-                  <Button
-                    id="get-started-button"
-                    variant="contained"
-                    color="primary"
-                    onClick={() => dispatch(hideSplashScreen())}
-                    endIcon={<i className="fas fa-arrow-right"></i>}
-                  >
-                    Get Started
-                  </Button>
-                </Col>
-              </Row>
-            </Container>
-          </Col>
-        </Row>
-      </Container>
+                      <InputLabel htmlFor="currency-select-field">
+                        Favorite Currency
+                      </InputLabel>
+                      <Select
+                        native
+                        value={currency}
+                        onChange={handleCurrencySelection}
+                        label="Favorite currency"
+                        inputProps={{
+                          name: 'currency',
+                          id: 'currency-select-field',
+                        }}
+                      >
+                        <option value={'USD'}>US Dollars</option>
+                        <option value={'CAD'}>Canadian Dollars</option>
+                        <option value={'EUR'}>Euros</option>
+                      </Select>
+                    </FormControl>
+                  </Col>
+                </Row>
+                <Row className="justify-content-center">
+                  <Col xs={10} md={6}>
+                    <Button
+                      id="get-started-button"
+                      variant="contained"
+                      color="primary"
+                      onClick={() => dispatch(hideSplashScreen())}
+                      endIcon={<i className="fas fa-arrow-right"></i>}
+                    >
+                      Get Started
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
